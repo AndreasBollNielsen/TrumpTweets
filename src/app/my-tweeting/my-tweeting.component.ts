692e3bd0f322d1a2d4cb7f979ab018794b9d70ce
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2022-01-25 15:44:10 +0100

    added ngfor

--- src/app/my-tweeting/my-tweeting.component.ts
+++ src/app/my-tweeting/my-tweeting.component.ts
@@ -5,16 +5,21 @@ import { TweetServiceService } from '../Services/tweet-service.service';
 @Component({
   selector: 'app-my-tweeting',
   templateUrl: './my-tweeting.component.html',
-  styleUrls: ['./my-tweeting.component.css']
+  styleUrls: ['./my-tweeting.component.css'],
 })
 export class MyTweetingComponent implements OnInit {
-
   datacollection: TweetModel[] = [];
-  constructor(private tweetService: TweetServiceService) { }
+  tagword: string = '';
+  constructor(private tweetService: TweetServiceService) {}
 
   ngOnInit(): void {
-
-    this.tweetService.GetTweets().subscribe((data: TweetModel[] )=> {next: this.datacollection = data});
+    this.tweetService.GetTweets().subscribe((data: TweetModel[]) => {
+      next: this.datacollection = data;
+    });
   }
 
+  checkWord(): boolean {
+    console.log('i do something');
+    return true;
+  }
 }
